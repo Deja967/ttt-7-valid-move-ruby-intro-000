ae20bcfb5b5709f6c3d79b32e4ede010d58b67b7
Deja Monae Allison <dejaallison@gmail.com>
2020-06-29 16:12:25 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,6 +7,6 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?
-  if index.between(0, 8)
+  if 1.between(0, 8)
   end
 end
