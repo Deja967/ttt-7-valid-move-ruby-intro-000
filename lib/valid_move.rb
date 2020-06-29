3ad7d4f9f096244b3fa451c35ed0dd9277a9fcb6
Deja Monae Allison <dejaallison@gmail.com>
2020-06-29 16:09:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
    if board[index] == " ".between?(0, 8)
+   end
   board[index] == "X"
   board[index] == "X" || board[index] == "O"
 end
